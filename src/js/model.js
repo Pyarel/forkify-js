38ceab7f1496367fa3b1aaca919498876b108523
Pyarel <pyarel.fer@gmail.com>
2023-11-24 12:12:59 -0500

    Refactoring MVC

--- src/js/model.js
+++ src/js/model.js
@@ -2,4 +2,33 @@ export const state = {
   recipe: {},
 };
 
-export const loadRecipe = async function () {};
+export const loadRecipe = async function (id) {
+  try {
+    // Loading Recipe
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034'
+    );
+
+    const data = await res.json();
+
+    // Guard clause - if response status is failed
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // console.log(data);
+
+    //Extract the recipe data
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
